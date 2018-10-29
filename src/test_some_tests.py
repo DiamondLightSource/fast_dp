dc79dad44b81c44f86c93c63d107e79ebbfd2edc
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-10-29 14:27:36 +0000

    use new location for regression data

--- src/test_some_tests.py
+++ src/test_some_tests.py
@@ -27,22 +27,15 @@
 # 6. more advanced stuff at https://docs.pytest.org/en/latest/
 
 import procrunner
-import libtbx.load_env
 import os
 import pytest
 
 def test_fast_dp_X4_wide(tmpdir):
+  X4_wide = '/dls/science/groups/scisoft/DIALS/regression_data/X4_wide'
+  if not os.path.exists(X4_wide):
+    pytest.skip('Could not find test image file')
 
-  src = os.path.join(libtbx.env.under_build('xia2_regression'),
-                     'test_data', 'X4_wide')
-  dls = '/dls/science/groups/scisoft/DIALS/repositories/current/' + \
-    'xia2_regression_data/test_data/X4_wide'
-  if not os.path.exists(src):
-    if not os.path.exists(dls):
-      pytest.skip('Could not find test image file')
-    src = dls
-
-  image = os.path.join(src, 'X4_wide_M1S4_2_0001.cbf')
+  image = os.path.join(X4_wide, 'X4_wide_M1S4_2_0001.cbf')
 
   bin = os.path.split(__file__)[0].replace('src', 'bin')
   fast_dp = os.path.join(bin, 'fast_dp')
