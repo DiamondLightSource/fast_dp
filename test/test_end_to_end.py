7b04225055fa6d15e81891098d051d5a069e3b94
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2018-10-30 16:00:31 +0000

    run fast_dp test directly

--- test/test_end_to_end.py
+++ test/test_end_to_end.py
@@ -1,24 +1,24 @@
 from __future__ import absolute_import, division, print_function
 
+import mock
 import os
-import procrunner
 import pytest
+import sys
 
-def test_fast_dp_X4_wide(tmpdir):
+def test_fast_dp_X4_wide(capsys, tmpdir):
   X4_wide = '/dls/science/groups/scisoft/DIALS/regression_data/X4_wide'
   if not os.path.exists(X4_wide):
     pytest.skip('Could not find test image file')
 
   image = os.path.join(X4_wide, 'X4_wide_M1S4_2_0001.cbf')
 
-  bin = os.path.split(__file__)[0].replace('src', 'bin')
-  fast_dp = os.path.join(bin, 'fast_dp')
-
-  cmd = [ fast_dp, '-a', 'Ba', image ]
+  cmd = [ 'fast_dp', '-a', 'Ba', image ]
   with tmpdir.as_cwd():
-    result = procrunner.run(cmd)
+    with mock.patch.object(sys, 'argv', cmd):
+      import fast_dp.fast_dp
+      fast_dp.fast_dp.main()
 
-  assert result['stderr'] == '', 'fast_dp output to STDERR:'
-  assert result['exitcode'] == 0, 'fast_dp non-zero exit code'
+  captured = capsys.readouterr()
+  assert captured.err == '', 'fast_dp output to STDERR:'
   for output in ['fast_dp.mtz', 'fast_dp.log']:
     assert tmpdir.join(output).check(), 'No output found'
