5ea3735997db024eefbb24294c4a89e68d7e152d
graeme-winter <graeme.winter@gmail.com>
2018-11-12 16:08:33 +0000

    Actually made work

--- fast_dp/diff.py
+++ fast_dp/diff.py
@@ -1,6 +1,42 @@
 from __future__ import absolute_import, division, print_function
 
-from fast_dp.image_readers import XDS_INP_to_dict
+def XDS_INP_to_dict(inp_text):
+    result = { }
+    for record in inp_text.split('\n'):
+        useful = record.split('!')[0].strip()
+        if not useful:
+            continue
+        if useful.count('=') > 1:
+            # assume tokens of form key=value key=value
+            tokens = useful.replace('=', ' ').split()
+            for j in range(useful.count('=')):
+                key = tokens[2*j].strip()
+                value = tokens[2*j+1].strip()
+                # handle multiples gracelessly
+                if key in result:
+                    if not isinstance(result[key], list):
+                        result[key] = [result[key]]
+                        result[key].append(value)
+                    else:
+                        result[key].append(value)
+                else:
+                    result[key] = value
+        else:
+            tokens = useful.split('=')
+            key = tokens[0].strip()
+            value = tokens[1].strip()
+            # handle multiples gracelessly
+            if key in result:
+                if not isinstance(result[key], list):
+                    result[key] = [result[key]]
+                    result[key].append(value)
+                else:
+                    result[key].append(value)
+            else:
+                result[key] = value
+
+    return result
+
 
 def diff(xds_inp_a, xds_inp_b):
     '''Compare the parameters in a, b; write out the differences'''
@@ -9,7 +45,7 @@ def diff(xds_inp_a, xds_inp_b):
 
     for key in all_keys:
         a = xds_inp_a.get(key, '')
-        b = xds_inp_b.get(key. '')
+        b = xds_inp_b.get(key, '')
         if a == b:
             continue
         print('%s:\n\t%s\n\t%s' % (key, a, b))
@@ -21,6 +57,6 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         raise RuntimeError, '%s XDS.INP ../other/XDS.INP' % sys.argv[1]
 
-    a = open(sys.argv[1]).read()
-    b = open(sys.argv[2]).read()
+    a = XDS_INP_to_dict(open(sys.argv[1]).read())
+    b = XDS_INP_to_dict(open(sys.argv[2]).read())
     diff(a, b)
