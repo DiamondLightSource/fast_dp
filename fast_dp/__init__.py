0327e28a82404690cd2932c6c3f43474b49414b3
rjgildea <richard.gildea@diamond.ac.uk>
2019-03-28 13:34:08 +0000

    Bump version: 1.2 → 1.3

--- fast_dp/__init__.py
+++ fast_dp/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import, division, print_function
 
 __author__ = """Diamond Light Source"""
 __email__ = "scientificsoftware@diamond.ac.uk"
-__version__ = "1.2"
+__version__ = "1.3"
