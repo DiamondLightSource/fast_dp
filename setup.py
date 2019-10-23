ecfdb5ae0f76f16e07baad300d6fd154424d9a49
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-10-23 15:13:04 +0100

    Bump version: 1.4 → 1.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     author="Diamond Light Source",
     author_email="scientificsoftware@diamond.ac.uk",
-    version="1.4",
+    version="1.5",
     url="https://github.com/DiamondLightSource/fast_dp",
     download_url="https://github.com/DiamondLightSource/fast_dp/releases",
     license="Apache-2.0",
